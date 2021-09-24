5c9aba611d9a481675d793bcd092cb38ca8a58df
Rino Lee <rinolee@google.com>
2021-09-24 01:53:05 -0700

    Resolve type mismatching bugs between given model weights and pruning variables of pruning api
    PiperOrigin-RevId: 398678080

--- tensorflow_model_optimization/python/core/sparsity/keras/pruning_impl.py
+++ tensorflow_model_optimization/python/core/sparsity/keras/pruning_impl.py
@@ -104,9 +104,9 @@ class Pruning(object):
       # Grab the (k-1)th value
 
       current_threshold = tf.gather(values, k - 1)
-      new_mask = tf.dtypes.cast(
-          tf.math.greater_equal(abs_weights, current_threshold), weights.dtype)
-    return current_threshold, new_mask
+      new_mask = tf.math.greater_equal(abs_weights, current_threshold)
+    return tf.dtypes.cast(current_threshold, weights.dtype), tf.dtypes.cast(
+        new_mask, weights.dtype)
 
   def _update_mask_sparsity_m_by_n(self, weights, m_by_n=(2, 4)):
     """Updates the m by n sparsity mask for a given weight tensor.
@@ -221,14 +221,16 @@ class Pruning(object):
     if tf.distribute.get_replica_context():
       values_and_vars = []
       for weight, mask, _ in self._pruning_vars:
-        masked_weight = tf.math.multiply(weight, mask)
+        masked_weight = tf.dtypes.cast(
+            tf.math.multiply(weight, mask), dtype=weight.dtype)
         values_and_vars.append((masked_weight, weight))
       if values_and_vars:
         assign_objs.append(tf.distribute.get_replica_context().merge_call(
             update_fn, args=(values_and_vars,)))
     else:
       for weight, mask, _ in self._pruning_vars:
-        masked_weight = tf.math.multiply(weight, mask)
+        masked_weight = tf.dtypes.cast(
+            tf.math.multiply(weight, mask), dtype=weight.dtype)
         assign_objs.append(tf_compat.assign(weight, masked_weight))
 
     return assign_objs
